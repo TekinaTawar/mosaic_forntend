5ee9b0be65268c469e72968e784f162ca4bc4598
TekinaTawar <astroaniket1@gmail.com>
2024-06-19 17:32:13 +0530

    inputs for home done

--- app/page.jsx
+++ app/page.jsx
@@ -64,18 +64,92 @@ const HomePage = () => {
             </li>
           </OptionGroup>
           <OptionGroup groupHead={"FABRIC ROLL"}>
-            <li className="width">
+            <div className="label-input-grid">
               <label htmlFor="width"> width</label>
               <div className="width-inputs">
-                <input type="text" />
-                <input type="radio" id="meter" name="unit" value="m" />
-                <label htmlFor="meter">m</label>
-                <input type="radio" id="inch" name="unit" value="in" />
-                <label htmlFor="inch">in</label>
+                <input type="number" />
+                <input
+                  type="radio"
+                  id="width-m"
+                  name="width-unit"
+                  value="m"
+                  defaultChecked
+                />
+                <label htmlFor="width-m">m</label>
+                <input
+                  type="radio"
+                  id="width-in"
+                  name="width-unit"
+                  value="in"
+                />
+                <label htmlFor="width-in">in</label>
               </div>
-            </li>
+              <label htmlFor="height">height</label>
+              <div className="height-inputs">
+                <input
+                  type="checkbox"
+                  id="height-enable"
+                  className="height-enable"
+                />
+                <label
+                  htmlFor="height-enable"
+                  className="height-enable-label"
+                ></label>
+                <input type="number" />
+                <input
+                  type="radio"
+                  id="height-m"
+                  name="height-unit"
+                  value="m"
+                  defaultChecked
+                />
+                <label htmlFor="height-m">m</label>
+                <input
+                  type="radio"
+                  id="height-in"
+                  name="height-unit"
+                  value="in"
+                />
+                <label htmlFor="height-in">in</label>
+              </div>
+              <label htmlFor="fabric-pattern">Fabric Pattern</label>
+              <div className="fabric-pattern-inputs">
+                <input
+                  type="checkbox"
+                  id="fabric-pattern-enable"
+                  className="fabric-pattern-enable"
+                />
+                <label
+                  htmlFor="fabric-pattern-enable"
+                  className="fabric-pattern-enable-label"
+                >
+                  Plane
+                </label>
+              </div>
+            </div>
           </OptionGroup>
-          <OptionGroup groupHead={"ARRANGEMENT SETTINGS"}></OptionGroup>
+          <OptionGroup groupHead={"ARRANGEMENT SETTINGS"}>
+            <div className="label-input-grid">
+              <label htmlFor="arrange-by" className="arrange-by-label">Arrange By</label>
+              <select name="arrange-by" id="arrange-by">
+                <option value="width">Width</option>
+                <option value="height">Height</option>
+                <option value="area">Area</option>
+              </select>
+              <label htmlFor="arrange-direction">Arrange Direction</label>
+              <select name="arrange-direction" id="arrange-direction">
+                <option value="horizontal">Horizontal</option>
+                <option value="vertical">Vertical</option>
+              </select>
+              <label htmlFor="fabric-orientation">Fabric Orientation</label>
+              <select name="fabric-orientation" id="fabric-orientation">
+                <option value="0">0</option>
+                <option value="90">90</option>
+                <option value="180">180</option>
+                <option value="270">270</option>
+              </select>
+            </div>
+          </OptionGroup>
         </ul>
         <button>Arrange Design</button>
       </section>
