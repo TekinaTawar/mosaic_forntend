4b3a29e47aa948e441f336cd3cb83905b4120a11
TekinaTawar <astroaniket1@gmail.com>
2024-06-20 12:13:13 +0530

    unsupported device message added

---
+++ app/components/UnsupportedDevice.jsx
@@ -0,0 +1,14 @@
+const UnsupportedDevice = () => {
+  return (
+    <div className="unsupported-device">
+      <div className="message">
+        <h1 className="mosaic">MOSAIC</h1>
+        <p>
+          Currently, Mosaic is not supported on small devices. Please open it on
+          a desktop.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default UnsupportedDevice;
