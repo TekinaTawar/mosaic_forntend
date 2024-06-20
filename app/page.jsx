72e60bf04fbeb118b4eb9257310a547ea57f35f1
TekinaTawar <astroaniket1@gmail.com>
2024-06-20 15:09:25 +0530

    rough design of pieces page done

--- app/page.jsx
+++ app/page.jsx
@@ -11,6 +11,7 @@ import {
 import { VscGoToFile, VscNewFile, VscNotebook } from "react-icons/vsc";
 import OptionGroup from "./components/OptionGroup";
 import PrimaryButton from "./components/PrimaryButton";
+import PiecesPanel from "./components/PiecesPanel";
 
 const HomePage = () => {
   return (
@@ -157,46 +158,49 @@ const HomePage = () => {
         <PrimaryButton />
       </section>
       <section className="content-section">
-        <section className="heading">
-          <h1>Mosaic</h1>
-          <span>By Vista</span>
-        </section>
-        <section className="start">
-          <h2>Start</h2>
-          <ul>
-            <li>
-              <VscGoToFile className="startIcons" />
-              <label htmlFor="openProject">Open Project</label>
-              <input type="file" id="openProject" />
-            </li>
-            <li>
-              <VscNewFile className="startIcons" />
-              <label htmlFor="newProject">New Project</label>
-              <input type="button" id="newProject" />
-            </li>
-            <li>
-              <VscNotebook className="startIcons" />
-              <label htmlFor="openDocs">Open Docs</label>
-            </li>
-          </ul>
-        </section>
-        <section className="about">
-          <h2>About</h2>
-          <p>
-            Mosaic is a software that takes in patterns as .dfx file for a cloth
-            design and arranges it on a sheet while optimizing it’s usage. This
-            is a subsidiary of VistaDesign.
-          </p>
-        </section>
-        <section className="recent">
-          <h2>Recent</h2>
-          <ul>
-            <li>
-              <span>file name</span>
-              <span>file location</span>
-            </li>
-          </ul>
-        </section>
+        {/* <div className="intitalContent">
+          <section className="heading">
+            <h1>Mosaic</h1>
+            <span>By Vista</span>
+          </section>
+          <section className="start">
+            <h2>Start</h2>
+            <ul>
+              <li>
+                <VscGoToFile className="startIcons" />
+                <label htmlFor="openProject">Open Project</label>
+                <input type="file" id="openProject" />
+              </li>
+              <li>
+                <VscNewFile className="startIcons" />
+                <label htmlFor="newProject">New Project</label>
+                <input type="button" id="newProject" />
+              </li>
+              <li>
+                <VscNotebook className="startIcons" />
+                <label htmlFor="openDocs">Open Docs</label>
+              </li>
+            </ul>
+          </section>
+          <section className="about">
+            <h2>About</h2>
+            <p>
+              Mosaic is a software that takes in patterns as .dfx file for a
+              cloth design and arranges it on a sheet while optimizing it’s
+              usage. This is a subsidiary of VistaDesign.
+            </p>
+          </section>
+          <section className="recent">
+            <h2>Recent</h2>
+            <ul>
+              <li>
+                <span>file name</span>
+                <span>file location</span>
+              </li>
+            </ul>
+          </section>
+        </div> */}
+        <PiecesPanel />
       </section>
     </>
   );
